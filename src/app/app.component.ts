69bef54260440826ba506f13f142fb63a82bf83a
vladimir9961 <wlada725@gmail.com>
2022-11-19 03:34:51 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
   error_message: string;
   registerForm = new FormGroup({
